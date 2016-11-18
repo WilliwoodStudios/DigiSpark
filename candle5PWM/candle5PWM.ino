f7de1b5a379c445f8bdc37e1a0604332f572fa49
Rob Williams <rob@williwoodstudios.com>
2016-11-17 20:54:01 -0500

    White noise delta

--- candle5PWM/candle5PWM.ino
+++ candle5PWM/candle5PWM.ino
@@ -1,45 +1,64 @@
-uint8_t values[6];
-uint8_t enabled[6];
+int8_t values[6];
 
-uint64_t globalTime;
+uint64_t time;
 
 uint8_t outputValues[64];
 
 
-ISR(TIMER0_OVF_vect) {
-    static uint8_t tick = 0;
-    DDRB = outputValues[tick++];
+ISR(TIMER0_COMPA_vect) {
+    static int8_t tick = 0;
+    static uint64_t globalTime = 0;
+    ++globalTime;
+    time = globalTime;
+    ++tick;
     tick&=0x3f;
+    DDRB = outputValues[tick];
+}
+
+void updateTable() {
+    for (int i=0; i<64; ++i) {
+        uint8_t toSet = 0;
+        for (int j=0; j<6; ++j) {
+            if (values[j]>i) {
+                toSet |= 1<<j;
+            }
+        }
+        outputValues[i] = toSet;
+    }
 }
 
 void setup() {
     // Disable pull ups.
     MCUCR |= 1<<PUD;
 
-    DDRB = PB1<<1;
-    PORTB = 1<<PB1;
-
-
-    OCR0A = 64;
-
-    TCCR0A = 0; // 1<<WGM01;
-    TCCR0B = 0 << CS02 | 1<<CS01 | 1 << CS00;
-    TIMSK = 1 << TOIE0;
-
     for (uint8_t i=0; i<6; ++i) {
-        values[i] = i*3;
-        enabled[i] = i!=5;
+        values[i] = 1;
     }
 
-    for (uint8_t i=0; i<64; ++i) {
-        outputValues[i] = i<4 ? 255 : 0;
-    }
+    updateTable();
+
+    DDRB = 255;
+    PORTB = 0;
+
+    OCR0A = 128;
+
+    TCCR0A = 1<<WGM01;
+    TCCR0B = 1 << CS02 | 0 <<CS01 | 1 << CS00;
+    TIMSK = 1 << OCIE0A;
+
 }
 
 void loop() {
-    // values[1]++;
-    // if (values[1] > 64) {
-    //     values[1] = 0;
-    // }
-//     PORTB |= 1 << PB1;
+    if (time % 1024 == 0) {
+        for (uint8_t i=0; i<6; ++i) {
+            int8_t toAdd = rand()%11 - 5;
+            values[i] += toAdd;
+            if (values[i] > 64) {
+                values[i] = 64;
+            } else if (values[i] < 10) {
+                values[i] = 10;
+            }
+        }
+        updateTable();
+    }
 }
