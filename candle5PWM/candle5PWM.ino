5a49e0f5d2331b3531bcd7e5dba25b8b2f76be1a
Rob Williams <rob@williwoodstudios.com>
2016-11-17 22:00:15 -0500

    Added harmonics and blue noise generation.

--- candle5PWM/candle5PWM.ino
+++ candle5PWM/candle5PWM.ino
@@ -1,9 +1,11 @@
-int8_t values[6];
+const uint8_t PIN_COUNT = 5;
+
+int8_t values[PIN_COUNT];
+float offsets[PIN_COUNT];
+float skews[PIN_COUNT];
+float harmonic[PIN_COUNT][3];
 
 uint64_t time;
-
-uint8_t outputValues[64];
-
 
 ISR(TIMER0_COMPA_vect) {
     static int8_t tick = 0;
@@ -12,32 +14,35 @@ ISR(TIMER0_COMPA_vect) {
     time = globalTime;
     ++tick;
     tick&=0x3f;
-    DDRB = outputValues[tick];
+    uint8_t mask = 1;
+    uint8_t toWrite = 0;
+    for (uint8_t i=0; i<PIN_COUNT; ++i) {
+        if (values[i] > tick) {
+            toWrite |= mask;
+        }
+        mask <<= 1;
+    }
+    DDRB = toWrite;
 }
 
-void updateTable() {
-    for (int i=0; i<64; ++i) {
-        uint8_t toSet = 0;
-        for (int j=0; j<6; ++j) {
-            if (values[j]>i) {
-                toSet |= 1<<j;
-            }
-        }
-        outputValues[i] = toSet;
-    }
+float findHarmonic(float center) {
+    return center * (1.0 + (rand()%21-10)/100);
 }
 
 void setup() {
+    for (uint8_t i=0; i<PIN_COUNT; ++i) {
+        values[i] = 1;
+        offsets[i] = rand()%1000/1000.0 * 6;
+        skews[i] = (100+(rand()%21-10))/100.0;
+        for (uint8_t j=0; j<3; ++j) {
+            harmonic[i][j] = findHarmonic(2<<j);
+        }
+    }
+
     // Disable pull ups.
     MCUCR |= 1<<PUD;
 
-    for (uint8_t i=0; i<6; ++i) {
-        values[i] = 1;
-    }
-
-    updateTable();
-
-    DDRB = 255;
+    DDRB = 0;
     PORTB = 0;
 
     OCR0A = 128;
@@ -48,17 +53,28 @@ void setup() {
 
 }
 
+float angle;
+float twoPi = 3.141597 * 2;
+
+uint8_t generateValueForAngle(float angle,uint8_t which) {
+    float toReturn = 0.6 * sin(angle);
+    toReturn += 0.1 * sin(angle * harmonic[which][0]);
+    toReturn += 0.1 * sin(angle * harmonic[which][1]);
+    toReturn += 0.2 * sin(angle * harmonic[which][2]);
+    toReturn *= 32;
+    toReturn += 32;
+    return toReturn;
+}
+
 void loop() {
     if (time % 1024 == 0) {
-        for (uint8_t i=0; i<6; ++i) {
-            int8_t toAdd = rand()%11 - 5;
-            values[i] += toAdd;
-            if (values[i] > 64) {
-                values[i] = 64;
-            } else if (values[i] < 10) {
-                values[i] = 10;
-            }
+        angle += 0.08;
+        if (angle > twoPi) {
+            angle -= twoPi;
         }
-        updateTable();
+
+        for (uint8_t i=0; i<PIN_COUNT; ++i) {
+            values[i] = generateValueForAngle((angle+offsets[i])*skews[i],i);
+        }
     }
 }
